cae05d849348f95e23db7f9c6e627ae44682fb77
Mitali <mitali.kawde11@gmail.com>
2023-04-25 10:26:51 -0700

    Instead of adding css in js file adding css class for styling css in seperate css file

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,17 +1,22 @@
+// Adding 'input' event handler on textarea
 $(document).ready(function() {
   $('#tweet-text').on('input', function () {
+    // getting the texts length 
     let charLeft = 140 - ($(this).val().length);
+    // getting 'output' (counter) element using ' this' traverse up and then go down to search 'output' ele
     let elementCounter = $(this).closest('form').find('.counter');
     
-    if(elementCounter) {
+    if (elementCounter) {
+      // updating counter 'output' value
       elementCounter.val(charLeft);
     }
 
-    if(elementCounter.val() < 0) {
-      $(elementCounter).css({'color': 'red'});
-    } else {
-      $(elementCounter).css({'color': '#4F4F4F'});
-    }
+    // if user exceeding char limit(140 char) then aading css class for diplaying negative val in red color
+    if (elementCounter.val() < 0) {
+      $(elementCounter).addClass("negativeVal");
 
+    } else {
+      $(elementCounter).removeClass("negativeVal");
+    }
   })
 });
