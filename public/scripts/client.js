ab47297dd48790c2158d4650693d4c72c3acd62f
Mitali <mitali.kawde11@gmail.com>
2023-05-02 03:44:25 -0700

    Creating HTML structure using jquery for tweet-container and rendering the tweets dynamically

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,68 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Create the HTML structure dynamically for the tweet
+const createTweetElement = function(tweetData) {
+  let $tweet = $(`
+    <article class="tweet">
+      <header>
+        <div>
+          <img src=${tweetData.user.avatars} alt="profile picture">
+          <div>${tweetData.user.name}</div>
+        </div>
+        <div class="handle">${tweetData.user.handle}</div>
+      </header>
+      <p>${tweetData.content.text}</p>
+      <footer>
+        <div>${tweetData.created_at}</div>
+        <div>
+          <i class="fa-solid fa-flag fa-xs"></i>
+          <i class="fa-solid fa-retweet fa-xs"></i>
+          <i class="fa-solid fa-heart fa-xs"></i>
+        </div>
+      </footer>
+    </article>
+  `);
+  
+  return $tweet;
+};
+
+const renderTweets = function(tweets) {
+  // loops through tweets
+  for (const tweet of tweets) {
+    // calls createTweetElement for each tweet
+    let $tweetHtml = createTweetElement(tweet);
+    // takes return value and appends it to the tweets container
+    $('#tweets-container').prepend($tweetHtml);
+  }
+};
+
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+$(document).ready(function() {
+  renderTweets(data);
+});
+
