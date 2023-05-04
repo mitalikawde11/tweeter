8e7d13e3ad7b037ded4b103c98b4e47045e2402e
Mitali <mitali.kawde11@gmail.com>
2023-05-03 18:39:37 -0700

    Fetching tweets with ajax instead of hard-coded tweets object and display the time passed since a Tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -17,7 +17,7 @@ const createTweetElement = function(tweetData) {
       </header>
       <p>${tweetData.content.text}</p>
       <footer>
-        <div>${tweetData.created_at}</div>
+        <div>${timeago.format(tweetData.created_at)}</div>
         <div>
           <i class="fa-solid fa-flag fa-xs"></i>
           <i class="fa-solid fa-retweet fa-xs"></i>
@@ -40,34 +40,7 @@ const renderTweets = function(tweets) {
   }
 };
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
 $(document).ready(function() {
-  renderTweets(data);
-
   // form submission using jquery
   // Add an event listener for submit(form) and prevent its default behaviour
   $('#new-tweet-submit').on('submit', function(event) {
@@ -85,5 +58,16 @@ $(document).ready(function() {
       console.log("Success: ", data);
     }) ;
   });
+
+  // fetching tweets from '/tweets' page using ajax GET request
+  const loadTweets = function () {
+    console.log("loading tweets...");
+    $.ajax("/tweets/", { method: "GET" })
+    .then(function (tweets) {
+      renderTweets(tweets);
+    });
+  };
+
+  loadTweets();
 });
 
